80fcf219643d2ac1eed504c83a888e6aa4e93e80
LeoPopolo <leopopologuitarreta@gmail.com>
2018-08-22 14:37:24 -0300

    ejercicio 2 C#
    Sello

---
+++ Ejercicio02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    using Entidad;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sello.mensaje = "Leo";
+            Sello.color = ConsoleColor.Blue;
+
+            Sello.ImprimirEnColor();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio02/Sello.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Sello
+    {
+        public static string mensaje;
+        public static ConsoleColor color;
+
+        public static string Imprimir()
+        {
+            return Sello.mensaje; 
+        }
+
+        public static void Borrar()
+        {
+            Sello.mensaje = "";
+        }
+
+        public static void ImprimirEnColor()
+        {
+            Console.ForegroundColor = Sello.color;
+            Console.WriteLine(Sello.Imprimir());
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
